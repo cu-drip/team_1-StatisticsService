ffb78e7dbcfe53f49e87c510c321674ca0397180
Alexander Pervanov <apervanov@gmail.com>
2025-07-12 20:07:16 +0300

    Update TournamentType.java

--- src/main/java/org/example/model/TournamentType.java
+++ src/main/java/org/example/model/TournamentType.java
@@ -1,8 +1,7 @@
 package org.example.model;
 
 public enum TournamentType {
-    round_robin,
-    single_elimination,
-    double_elimination,
-    group_stage
+    olympic,
+    swiss,
+    round_robin
 } 
